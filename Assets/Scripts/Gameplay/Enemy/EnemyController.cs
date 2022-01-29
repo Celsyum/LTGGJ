66657327e4a2f451adca0bf1912db61b073f69e8
Tautvydas <tautvydas.svarinskas@gmail.com>
2022-01-29 12:01:29 +0200

    Added healthbar to blank scene

--- Assets/Scripts/Gameplay/Enemy/EnemyController.cs
+++ Assets/Scripts/Gameplay/Enemy/EnemyController.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class EnemyController : StateMashine
 {
-    void Start()
+    [Header("Attributes")]
+    [SerializeField] private float health;
+
+    public Transform Target { get; private set; }
+
+    private void Start()
     {
+        Target = GameManager.Instance.Player.transform;
+
         SetState(new EnemySpawn(this));
     }
 
     protected override void Update()
     {
         base.Update();
+    }
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        } 
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public bool IsTargerVisible()
+    {
+        RaycastHit2D hit;
+        Vector3 rayDirection = Target.position - transform.position;
+        hit = Physics2D.Raycast(transform.position, rayDirection);
+        Debug.Log(hit.transform.name);
+        return hit.transform == Target;
     }
 }
